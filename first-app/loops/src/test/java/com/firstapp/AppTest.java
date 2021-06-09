cf3ecd2d741b354ad8ff752d0fe16b8d9efdbda4
tehbat25 <85258668+tehbat25@users.noreply.github.com>
2021-06-08 22:11:56 -0400

    Add files via upload

---
+++ first-app/loops/src/test/java/com/firstapp/AppTest.java
@@ -0,0 +1,15 @@
+package com.firstapp;
+
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+ /**    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }*/
+}
